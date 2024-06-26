7b61a434de9b178534564eaf841487e85ba44c4a
yossef-ashraf <yossefff2001@gmail.com>
2024-06-26 10:48:46 +0300

    Update Big-O Cheat Sheet with time complexity examples

--- S1_Algorithmic.py
+++ S1_Algorithmic.py
@@ -20,56 +20,6 @@ def binary(L, x):
         else:
             return binary(L[mid+1:], x)
 
-####################################################################################################################################################
-
-# calcolit BIG O
-# o(1) < o(log(n)) < o(root n) < o(n) < o(nlog(n)) < o(n^2) < o(2^n) < o(n!) < o(n^n)
-
-# for(i=1; i<=2*n;i++)
-# big o here is o(n) 
-#####################################
-# for(i=3; i<=n;i*=2)
-# 2^3 = 8 
-# i = log2(8) = 3 
-# 1 - 2 - 4 - 8 - 16 ....
-# big o here is o(log2(n)) 
-#####################################
-# for(i=1; i<=n;i*=3)
-# big o here is o(log3(n)) 
-#####################################
-# for(i=1; i<=n;i+=2)
-# 1-3-5-7-9... (n/2)
-# big o here is o(n)
-#####################################
-# for(i=1; i<=n;i++)
-#     for(j=1; j<= 1000 ; j++)
-# big o here is o(n) // n+1000n
-##################################### 
-# for(i=1;i<=n;i++)
-#   for(j=1; j<=j+3 ; j++) // 1-2-3
-# big o here is o(n) 
-##################################### 
-# for(i=1;i<=n;i++)
-#   for(j=1; j<= n*n ; j+=2)
-# big o here is o(n^3) 
-##################################### 
-# for(i=1;i<=n;i++)
-#   for(j=1; j<= n; j*=2)
-# big o here is o(nlog2(n)) 
-##################################### 
-# for(i=1;i<=n;i++)
-#   for(j=1; j<= n*n; j*=2)
-# n+(n*log2(n^2)) 
-# big o here is o(n^2) 
-
-# Storing data in algroithm
-# c in rigster
-# 3 c in cache
-# 100 - 1000 c in ram 
-# 10^6 c < in hard 
-
-####################################################################################################################################################
-
 # Sorting Algroithm
 def bubbleSort(arr):
     n = len(arr)
@@ -92,98 +42,82 @@ def bubbleSort(arr):
 
 
 
+####################################################################################################################################################
+### Storing Data in Algorithms
+# - Register: 1 value
+# - Cache: 3 values
+# - RAM: 100-1000 values
+# - Hard Disk: 10⁶ values
+
+
+### Examples of Big O Calculations
+
+####  1
+
+# for(i = 1; i <= 2 * n; i++)
+# - Big O: O(n)
+
+# ####  2
+
+# for(i = 3; i <= n; i *= 2)
+# - Big O: O(log₂(n))
+
+# ####  3
+
+# for(i = 1; i <= n; i *= 3)
+# - Big O: O(log₃(n))
+
+# ####  4
+
+# for(i = 1; i <= n; i += 2)
+# - Big O: O(n)
+
+# ####  5
+
+# for(i = 1; i <= n; i++)
+#     for(j = 1; j <= 1000; j++)
+# - Big O: O(n)
+
+# ####  6
+
+# for(i = 1; i <= n; i++)
+#     for(j = 1; j <= j + 3; j++)
+# - Big O: O(n)
+
+# ####  7
+
+# for(i = 1; i <= n; i++)
+#     for(j = 1; j <= n * n; j += 2)
+# - Big O: O(n³)
+
+# ####  8
+
+# for(i = 1; i <= n; i++)
+#     for(j = 1; j <= n; j *= 2)
+# - Big O: O(n log₂(n))
+
+# ####  9
+
+# for(i = 1; i <= n; i++)
+#     for(j = 1; j <= n * n; j *= 2)
+# - Big O: O(n²)
+
+
+# calcolit BIG O
+# - O(1) < O(log(n)) < O(√n) < O(n) < O(n log(n)) < O(n²) < O(2ⁿ) < O(n!) < O(nⁿ)
+
+# Big-O 
+
+# | Big-O      | Name             | Description                                               | Example                             |
+# |------------|------------------|-----------------------------------------------------------|-------------------------------------|
+# | O(1)       | Constant Time    | Execution time remains unchanged irrespective of input data | Checking if a stack is empty        |
+# | O(log n)   | Logarithmic Time | Complexity increases by one unit for each doubling of input data | Finding an item in a balanced search tree |
+# | O(n)       | Linear Time      | Execution time increases linearly with the size of the input data | Linear traversal of a list          |
+# | O(n log n) | Log-Linear Time  | Complexity grows as a combination of linear and logarithmic | Merge sort on a collection of items |
+# | O(n²)      | Quadratic Time   | Time taken is proportional to the square of the number of elements | Checking all possible pairs in an array |
+# | O(n³)      | Cubic Time       | Execution time is proportional to the cube of the number of elements | Matrix multiplication of n x n matrices |
+# | O(2ⁿ)      | Exponential Time | Time doubles for every new element added                   | Generating all subsets of a given set |
+# | O(n!)      | Factorial Time   | Complexity grows factorially based on the size of the dataset | Determining all permutations of a given list |
 
 
 
-
-
-
-
-
-
-
-
-
-
-Big-o Cheat Sheet
-
-Big-o       Name        Description        Example
-
-0(1)      Constant Time    Checking if a stack is empty     Execution time remains unchanged irrespective of input data
-
-0(log n)    Logarithmic Time    Complexity increases by one unit for each doubling of input data    Finding an item in a balanced search tree
-
-O(n)
-
-Linear
-Time
-
-Execution time increases
-linearly with the size of the
-input data
-
-Linear traversal
-of a list
-
-O(n log n)
-
-Log-Linear
-Time
-
-Complexity grows as a
-combination of linear and
-logarithmic
-
-Merge sort on
-a collection of
-items
-
-O(n^2)
-
-auadratic
-Time
-
-Time taken is proportional to
-the square ofthe number of
-elements
-
-Checking all
-possible pairs in
-an array
-
-O(n^3)
-
-Cubic Time
-
-Execution time is proportional
-to the cube ofthe number of
-elements
-
-Matrix
-multiplication of
-n x n matrices
-
-O(2^n)
-
-Exponential
-Time
-
-Time doubles for every new
-element added
-
-Generating all
-subsets of a
-given set
-
-O(n!)
-
-Factorial
-Time
-
-Complexity grows factorially
-based on the size ofthe
-dataset
-
-Determining all
-permutations of
-a given list
